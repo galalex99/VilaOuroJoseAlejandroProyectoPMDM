0b0c144cd8aac34f37ac52ee63ed473aa5578f8b
galalex99 <alex.vila.ouro@gmail.com>
2021-10-15 13:21:52 +0200

    Proyecto inicial

--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/MainActivity.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/MainActivity.kt
@@ -1,11 +1,20 @@
 package ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvClickRegistro : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvClickRegistro= findViewById( R.id.tvClickRegistro)
+        tvClickRegistro.setOnClickListener(){
+        val intent= Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
