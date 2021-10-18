5d7dccf7dcb64427a3623f81b5d3618a8ffb8621
galalex99 <alex.vila.ouro@gmail.com>
2021-10-18 10:27:16 +0200

    Login e rexistro finalizados inicio de shared preferences

--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/MainActivity.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val preferenciasApp = getSharedPreferences("Preferencias",MODE_PRIVATE)
         tvClickRegistro= findViewById( R.id.tvClickRegistro)
         tvClickRegistro.setOnClickListener(){
         val intent= Intent(this,MainActivity2::class.java)
--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/MainActivity2.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/MainActivity2.kt
@@ -2,12 +2,18 @@ package ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var btnRegistro : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        btnRegistro= findViewById( R.id.btnRegistro)
+        btnRegistro.setOnClickListener(){
+            super.onBackPressed()
+        }
 
-        //onbackpressed
     }
 }
