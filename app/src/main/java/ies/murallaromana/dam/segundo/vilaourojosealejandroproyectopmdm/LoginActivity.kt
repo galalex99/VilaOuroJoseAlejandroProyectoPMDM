06a3217201ab81737345363188e793d73e0b2e72
galalex99 <alex.vila.ouro@gmail.com>
2021-11-02 10:27:01 +0100

    InicioListaPeliculas

--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/LoginActivity.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/LoginActivity.kt
@@ -5,16 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private lateinit var tvClickRegistro : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         val preferenciasApp = getSharedPreferences("Preferencias",MODE_PRIVATE)
         tvClickRegistro= findViewById( R.id.tvClickRegistro)
         tvClickRegistro.setOnClickListener(){
-        val intent= Intent(this,MainActivity2::class.java)
+        val intent= Intent(this,RegisterActivity::class.java)
             startActivity(intent)
         }
     }
