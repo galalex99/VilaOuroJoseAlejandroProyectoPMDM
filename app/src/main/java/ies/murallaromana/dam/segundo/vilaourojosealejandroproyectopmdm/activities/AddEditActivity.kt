bdfa697ebb3dd88865d994f9800cdb6abd701cff
galalex99 <alex.vila.ouro@gmail.com>
2021-11-26 14:18:36 +0100

    Some of the button functions of the menu working

--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/activities/AddEditActivity.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/activities/AddEditActivity.kt
@@ -2,12 +2,47 @@ package ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.activiti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.R
+import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.model.entities.Film
 
 class AddEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit)
+        if (intent.extras?.get("film") == null) {
 
+        } else {
+            val pelicula = intent.extras?.get("film") as Film
+            title = "Editar ${pelicula.title}"
+        }
+    }
+
+    // Initialize menu buttons
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.films_detail_menu, menu)
+        if (menu != null) {
+            DetailActivity.menuItemSave = menu.findItem(R.id.save_action)
+            DetailActivity.menuItemDelete = menu.findItem(R.id.delete_action)
+            DetailActivity.menuItemEdit = menu.findItem(R.id.edit_action)
+        }
+        return true
+    }
+
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if (intent.extras?.get("film") != null) {
+            DetailActivity.menuItemEdit.isVisible = false
+            DetailActivity.menuItemSave.isVisible = true
+            DetailActivity.menuItemDelete.isVisible = true
+
+        }else{
+            DetailActivity.menuItemEdit.isVisible = false
+            DetailActivity.menuItemSave.isVisible = true
+            DetailActivity.menuItemDelete.isVisible = false
+        }
+
+
+        return super.onPrepareOptionsMenu(menu)
     }
 }
--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/activities/DetailActivity.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/activities/DetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.squareup.picasso.Picasso
 import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.R
 import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.databinding.ActivityDetailBinding
@@ -17,15 +18,20 @@ class DetailActivity : AppCompatActivity() {
         lateinit var menuItemSave: MenuItem
         lateinit var menuItemDelete: MenuItem
         lateinit var menuItemEdit: MenuItem
+        lateinit var infoFilm: Film
 
     }
 
     private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
         val infoFilm = intent.extras?.get("film") as Film
         title = infoFilm.title
         Picasso.get().load(infoFilm.url).into(binding.ivFilmImage)
@@ -36,32 +42,47 @@ class DetailActivity : AppCompatActivity() {
         binding.tvDetailFilmLanguage.text = infoFilm.language
         binding.tvDetailFilmMoviePremiere.text = infoFilm.moviePremiere
         binding.ratingBarDetail.rating = (infoFilm.score / 2).toFloat()
-
     }
 
     // Initialize menu buttons
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.films_detail_menu, menu)
         if (menu != null) {
-            menuItemSave= menu.findItem(R.id.save_action)
-            menuItemDelete= menu.findItem(R.id.delete_action)
-            menuItemEdit= menu.findItem(R.id.edit_action)
+            menuItemSave = menu.findItem(R.id.save_action)
+            menuItemDelete = menu.findItem(R.id.delete_action)
+            menuItemEdit = menu.findItem(R.id.edit_action)
         }
         return true
     }
 
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        if(intent.extras?.get("film")!=null){
-            menuItemEdit.isEnabled=true
-            menuItemEdit.isEnabled=true
-            menuItemEdit.isEnabled=true
+        if (intent.extras?.get("film") != null) {
+            menuItemEdit.isVisible = true
+            menuItemDelete.isVisible = true
+            menuItemSave.isVisible = false
 
         }
-
-
         return super.onPrepareOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.delete_action -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Borrado Pelicula")
+                    .setMessage("Estas seguro de borrar ${infoFilm.title}, No se podra revertir el borrado")
+                    .setPositiveButton(
+                        android.R.string.ok
+                    ) // After clicking the accept button we remove the film
+                    { _, _ ->
+
+                    }.show()
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
