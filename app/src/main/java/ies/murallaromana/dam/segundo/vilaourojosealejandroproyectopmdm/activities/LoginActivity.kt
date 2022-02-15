bbce2c4d148c40a1962c8d9e859b269bbfd32f38
galalex99 <alex.vila.ouro@gmail.com>
2022-02-15 09:38:16 +0100

    App finished

--- app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/activities/LoginActivity.kt
+++ app/src/main/java/ies/murallaromana/dam/segundo/vilaourojosealejandroproyectopmdm/activities/LoginActivity.kt
@@ -1,11 +1,14 @@
 package ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.activities
 
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.isVisible
 import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.R
 import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.RetrofitClient
 import ies.murallaromana.dam.segundo.vilaourojosealejandroproyectopmdm.databinding.ActivityLoginBinding
@@ -41,8 +44,6 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.tietEmailLogin.setText("alex5@gmail.com")
-        binding.tietPasswdLogin.setText("abc123..")
         // use binding to get the button and add an onclickListeners
         binding.tvRegisterClick.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
@@ -55,11 +56,13 @@ class LoginActivity : AppCompatActivity() {
             val user = User(binding.tietEmailLogin.text.toString(),
                 binding.tietPasswdLogin.text.toString())
             binding.btLogin.isEnabled=false
+            binding.progressBar.visibility= View.VISIBLE
             val loginCall = RetrofitClient.apiRetrofit.login(user)
             loginCall.enqueue(object : Callback<Token> {
                 override fun onFailure(call: Call<Token>, t: Throwable) {
                     Log.d("Login OnFailure", t.toString())
                     binding.btLogin.isEnabled=true
+                    binding.progressBar.visibility= View.INVISIBLE
                 }
 
                 override fun onResponse(call: Call<Token>, response: Response<Token>) {
@@ -72,6 +75,7 @@ class LoginActivity : AppCompatActivity() {
                                 "Error gardando o token",
                                 Toast.LENGTH_SHORT).show()
                             binding.btLogin.isEnabled=true
+                            binding.progressBar.visibility= View.INVISIBLE
                         } else {
                             preferences.saveData(token)
                             Log.d("LoginC", "Login correcto")
